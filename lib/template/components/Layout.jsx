891ea8811cae8c623937c7270f111295126a3f3a
Bun <bunwong@qq.com>
2020-10-19 10:43:47 +0800

    rename service filename

--- lib/template/components/Layout.jsx
+++ lib/template/components/Layout.jsx
@@ -4,7 +4,7 @@ import { withApp } from '@/utils/withapp';
 @withApp('__appid__')
 class __Appid__Layout extends React.PureComponent {
   render() {
-    // 可以通过 this.props.app.location?.pathname 来判断 location 路径
+    // 可以通过 this.props.app.location.pathname 来判断 location 路径，以渲染不同的 layout
 
     return this.props.children;
   }
